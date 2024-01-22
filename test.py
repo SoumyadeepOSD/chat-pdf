b6a23acaba07751f3df630284a47349fcef7c373
SoumyadeepOSD <opencontainer255@gmail.com>
2024-01-22 09:08:26 +0530

    Added Gitignore

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-from dotenv import load_dotenv
-import streamlit as st
-from PyPDF2 import PdfReader
+from langchain.chains.question_answering import load_qa_chain
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
 from langchain_community.llms import OpenAI
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+import streamlit as st
 import os
 
 
@@ -13,17 +13,22 @@ def main():
     load_dotenv()
     OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
     st.set_page_config("Chat with multiple PDF")
-    st.header("Ask your pdf")
+    st.header("Chat with PDF")
 
     pdf = st.file_uploader("Upload PDF", type="pdf", accept_multiple_files=False)
 
-    if pdf is not None:
-        reader = PdfReader(pdf)
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text()
+
+    # Get PDF text
+    def get_pdf_text(pdf):
+        if pdf is not None:
+            reader = PdfReader(pdf)
+            text = ""
+            for page in reader.pages:
+                text += page.extract_text()
+            return text
         
-        # Splitting into chunks
+    # Get Text chunks
+    def get_text_chunk(text):
         text_splitter = CharacterTextSplitter(
             separator="\n",
             chunk_size=1000,
@@ -31,23 +36,31 @@ def main():
             length_function=len,
         )
         chunks = text_splitter.split_text(text)
-        
-        #Openai Embeddings
-        embeddings = OpenAIEmbeddings(
+        return chunks
+    
+    pdfToText = get_pdf_text(pdf)
+    textChunks = get_text_chunk(pdfToText)
+    
+    
+    # Creating OpenAI Embeddings
+    embeddings = OpenAIEmbeddings(
             openai_api_key=OPENAI_API_KEY,
         )
-        knowledge_base = FAISS.from_texts(chunks, embeddings)
+    knowledge_base = FAISS.from_texts(textChunks, embeddings)
+        
+        
+        
         
         #user question
-        user_question = st.text_input("Ask a question about your pdf")
-        if user_question is not None:
-            trigger = st.button("Submit")
-            if trigger:
-                docs = knowledge_base.similarity_search(user_question)
-                llm = OpenAI()
-                chain = load_qa_chain(llm, chain_type="stuff")
-                response = chain.run(input_documents=docs, question=user_question)
-                st.write(response)
+    user_question = st.text_input("Ask a question about your pdf")
+    if user_question is not None:
+        trigger = st.button("Submit")
+        if trigger:
+            docs = knowledge_base.similarity_search(user_question)
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+            response = chain.run(input_documents=docs, question=user_question)
+            st.write(response)
         
 if __name__ == "__main__":
     main()
